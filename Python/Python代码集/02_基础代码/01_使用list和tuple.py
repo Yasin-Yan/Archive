1a60e4dc80537717e0881949896dc17586b037e9
Yasin-Yan <W15797262726@outlook.com>
2020-10-19 17:19:01 +0800

    2020年10月19日学习

---
+++ Python/Python代码集/02_基础代码/01_使用list和tuple.py
@@ -0,0 +1,14 @@
+# 列表
+# classmates = ['Jack', 'Mary', 'Tomas']
+# classmates.append('Adam') # 尾部添加
+# print(classmates[-1])
+# classmates.insert(1, 'Tracy')
+# classmates.pop() # 尾部删除
+# classmates.pop(2) # 指定删除
+# classmates[1] = 'Sarah'
+# print(classmates)
+
+# 元组
+p = (2,) #定义只有一个元素的元组
+print(p)
+
---
+++ Python/Python代码集/02_基础代码/03_使用dict和set.py
@@ -0,0 +1,22 @@
+# # dict
+# d = {
+#     'Jack': 100,
+#     'Rose': 85,
+#     'Rowling': 72
+# }
+# # print(d['Rose'])
+# # print('Thomas' in d)
+# # print(d.get('Thomas', -1))
+# d.pop('Rose')
+# print(d)
+
+# set
+s = set([2, 2, 3, 3, 4, 4])
+s.add(5)
+s.remove(4)
+print(s)
+
+# a = [6, 4, 5, 2, 3]
+# print(a)
+# a.sort()
+# print(a)
